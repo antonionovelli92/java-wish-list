2937074882529a45ce54de2c9d88af40fa76d05b
antonionovelli92 <anto.novelli@hotmail.it>
2023-05-12 11:43:40 +0200

    first commit

---
+++ src/org/lessons/java/christmas/Main.java
@@ -0,0 +1,30 @@
+package org.lessons.java.christmas;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> wishList = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        boolean add = true;
+        do {
+            System.out.print("Inserisci un desiderio: ");
+            String newWish = sc.nextLine();
+            wishList.add(newWish);
+            
+            System.out.println("Lunghezza della lista dei desideri: " + wishList.size());
+            
+            System.out.print("Vuoi continuare ad inserire desideri? (S/N): ");
+            String continua = sc.nextLine();
+            add = continua.toLowerCase().equals("s");
+        } while (add);
+
+        System.out.println("Va bene, ecco la lista dei tuoi desideri:");
+        for (String wish : wishList) {
+            System.out.println("- " + wish);
+        }
+        sc.close();
+   }
+}
